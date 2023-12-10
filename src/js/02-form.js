43dd88837864828b76030b107d325d428288670e
Costbound <popovskiy96@gmail.com>
2023-12-10 23:55:57 +0100

    correction-1

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -1,2 +1,40 @@
 const form = document.querySelector("form");
-export default form;
+
+const localStorageKey = "feedback-form-state";
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
+
+const inputsOnPageLoad = async () => {
+    try {
+        const localStorageValue = JSON.parse(localStorage.getItem(localStorageKey));
+        if (localStorageValue) {
+            emailInput.value = localStorageValue.email;
+            messageInput.value = localStorageValue.message;
+        }
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+inputsOnPageLoad();
+
+form.addEventListener("input", onInputsUpdate);
+form.addEventListener("submit", onSubmit);
+
+async function onInputsUpdate() {
+    const inputsValues = {
+        email: emailInput.value.trim(),
+        message: messageInput.value.trim()
+    }
+    localStorage.setItem(localStorageKey, JSON.stringify(inputsValues));
+}
+
+function onSubmit(evt) {
+    evt.preventDefault();
+    const userMessage = {
+        email: emailInput.value,
+        message: messageInput.value
+    }
+    console.log(userMessage);
+    localStorage.removeItem(localStorageKey);
+    evt.target.reset();
+}
